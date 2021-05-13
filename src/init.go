1a255bbeeba791e1676c54bdc0521c1864b6b48e
hrushikeshk <hru123.kakade@gmail.com>
2021-05-13 14:14:29 +0530

    Disable database

--- src/init.go
+++ src/init.go
@@ -6,12 +6,12 @@ import (
 
 var database *sql.DB
 
-func init() {
-	// Create DB
-	database, _ = sql.Open("sqlite3", "../data/db/user_data.db")
-	statement, _ := database.Prepare("CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY, UUID TEXT, emails TEXT, date TEXT, pincode TEXT, ageGroup INTEGER)")
-	statement.Exec()
+// func init() {
+// Create DB
+// database, _ = sql.Open("sqlite3", "../data/db/user_data.db")
+// statement, _ := database.Prepare("CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY, UUID TEXT, emails TEXT, date TEXT, pincode TEXT, ageGroup INTEGER)")
+// statement.Exec()
 
-	// Create workers for users already in DB
-	// initDB()
-}
+// Create workers for users already in DB
+// initDB()
+// }
